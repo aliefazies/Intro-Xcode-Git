805e299ac0149fbb60209bdf4b58a631309d29f3
Alief Ahmad Azies <alief.ahmadazies@gmail.com>
2023-04-04 09:52:07 +0700

    Initial Commit

---
+++ intro-xcode-git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  intro-xcode-git
+//
+//  Created by Alief Ahmad Azies on 04/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
